781a0501977cd6130f8aea38dee9327471dbea94
bernadette privateer <lynprivateer@gmail.com>
2017-08-24 17:47:01 -0400

    changed to new thursday project

--- index.js
+++ index.js
@@ -1,33 +1,77 @@
 const express = require("express");
-const path = require("path");
 const mustacheExpress = require("mustache-express");
-const bodyParser = require("body-parser");
+const path = require("path");
+const routes = require("./routes/user");
 const morgan = require("morgan");
-const session = require("express-session");
-const routes = require("./routes/user.js");
-const expressValidator = require("express-validator");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const flash = require('express-flash-messages');
+const User = require("./models/user");
+
 const app = express();
 
-app.engine("mustache", mustacheExpress());
-app.set("views", path.join(__dirname, "./views"));
-app.set("view engine", "mustache");
-app.set("layout", "layouts");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.static(path.join(__dirname, "./public")));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(expressValidator());
+app.engine("mustache", mustacheExpress());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "mustache");
+app.set("layout", "layout");
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 app.use(morgan("dev"));
 
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.authenticate(username, password, function(err, user) {
+            if (err) {
+                return done(err)
+            }
+            if (user) {
+                return done(null, user)
+            } else {
+                return done(null, false, {
+                    message: "There is no user with that username and password."
+                })
+            }
+        })
+    }));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+})
+
 app.use(session({
-  secret: "mobbinbob",
-  resave: false,
-  saveUninitialized: false
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new(require('express-sessions'))({
+        storage: 'mongodb'
+    })
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.use(routes);
 
 app.listen(3000, function() {
-  console.log("were in!");
+  console.log("App is running on localhost:3000");
 });
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,29 @@
-const express = require('express');
+const express = require("express");
+const User = require("../models/user");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require('passport');
 
-let data = [];
+mongoose.connect("mongodb://localhost:27017/robots");
 
+
+// let db;
+//
+// let data = [];
+const getListings = function(req, res, next){
+  User.find({}).sort("name")
+  .then(function(users){
+    console.log(users);
+    req.session.users = users;
+    next();
+  })
+  .catch(function(err){
+    console.log(err);
+    next(err);
+  })
+
+}
+//
 // let getData = function(db, callback) {
 //   let users = db.collection('users');
 //
@@ -11,38 +32,80 @@ let data = [];
 //     callback();
 //   });
 // };
+//
+//
+//    getData(db, function() {
+//       db.close();
+//       next();
+//     });
 
-const getListings = function(req, res, next){
-  let MongoClient = require('mongodb').MongoClient;
-  let assert = require('assert');
-
- let url = "mongodb://localhost:27017/robots";
 
 
- MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
 
-   getData(db, function() {
-      db.close();
-      next();
-    });
-  });
+router.get('/',  getListings, function(req, res){
 
-  let getData = function(db, callback) {
-    let users = db.collection('users');
+  res.render("index", {users: req.session.users})
+});
 
-   users.find({}).toArray().then(function(users) {
-      data = users;
-      callback();
-    });
-  };
 
+
+const requireLogin = function (req, res, next) {
+  if (req.user) {
+    console.log(req.user)
+    next()
+  } else {
+    res.redirect('/');
+  }
 };
 
-router.get('/', getListings, function(req, res){
-  res.render("index", {users: data})
-})
+const login = function (req, res, next) {
+  if (req.user) {
+    res.redirect("/user")
+  } else {
+    next();
+  }
+};
 
+router.get("/", login, function(req, res) {
+
+
+  res.render("index", {
+      messages: res.locals.getMessages()
+  });
+});
+
+router.post('/', passport.authenticate('local', {
+    successRedirect: '/user',
+    failureRedirect: '/',
+    failureFlash: true
+}));
+
+router.get("/signup", function(req, res) {
+  res.render("signup");
+});
+
+router.post("/signup", function(req, res) {
+  User.create({
+    username: req.body.username,
+    password: req.body.password
+  }).then(function(data) {
+    console.log(data);
+    res.redirect("/");
+  })
+  .catch(function(err) {
+    console.log(err);
+    res.redirect("/signup");
+  });
+});
+
+router.get("/user", requireLogin, function(req, res) {
+  res.render("user", {username: req.user.username});
+});
+
+router.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
 
 
 module.exports = router;
